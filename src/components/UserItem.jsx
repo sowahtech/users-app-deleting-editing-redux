02c932e9c2d0ece107f8fd27fc1149daa7c36a32
justice sowah <justice.sowah18@gmail.com>
2021-03-24 11:48:50 +0000

    deleteUser method added to app as a prop

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -6,12 +6,15 @@ import React from "react";
 // import React, {Component} from "react".
 
 
-export default function UserItem({name, email}) {
+export default function UserItem({name, email, removeUser}) {
+	const handleClick = () => {
+		removeUser(name);
+	}
 	return (
 		<div>
 			<h1>{name}</h1>
 			<h3>{email}</h3>
-			<button style={{borderRadius: 10, backgroundColor:"#6FF8FC"}}>Remove User</button>
+			<button style={{borderRadius: 10, backgroundColor:"#6FF8FC"}} onclick = {handleClick}>Remove User</button>
 			<hr style={{backgroundColor: "#B05E2A", height: 5}} />
 		</div>
 	)
